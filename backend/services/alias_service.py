7d24aad107ff34590c85dc5aa252fdc58886b68c
yrmint <zhilkinal@yandex.ru>
2025-12-24 21:51:55 +0300

    column aliases + minor fixes

--- backend/services/alias_service.py
+++ backend/services/alias_service.py
@@ -39,6 +39,14 @@ def create_db_alias(name, alias):
 
         db_row = meta_cursor.fetchone()
         if db_row:
+            alias_cursor.execute("SELECT db_meta_id "
+                                 "FROM db_aliases "
+                                 "WHERE db_meta_id = %s",
+                                 (db_row["db_id"], ) )
+
+            if alias_cursor.fetchone():
+                raise HTTPException(status_code=400, detail="Database already has alias")
+
             alias_cursor.execute(
                 "INSERT INTO db_aliases (db_meta_id, db_alias_name) VALUES (%s, %s);",
                 (db_row["db_id"], alias, )
@@ -90,6 +98,14 @@ def create_table_alias(database, name, alias):
 
         db_row = meta_cursor.fetchone()
         if db_row:
+            alias_cursor.execute("SELECT table_meta_id "
+                                 "FROM table_aliases "
+                                 "WHERE table_meta_id = %s",
+                                 (db_row["table_id"], ) )
+
+            if alias_cursor.fetchone():
+                raise HTTPException(status_code=400, detail="Column already has alias")
+
             alias_cursor.execute(
                 "INSERT INTO table_aliases (table_meta_id, table_alias_name) VALUES (%s, %s);",
                 (db_row["table_id"], alias, )
@@ -104,17 +120,80 @@ def create_table_alias(database, name, alias):
     alias_conn.close()
 
 
+def create_column_alias(database, table, name, alias):
+    meta_conn = get_connection()
+    meta_cursor = meta_conn.cursor(dictionary=True)
+
+    alias_conn = get_connection("query_aliases")
+    alias_cursor = alias_conn.cursor(dictionary=True)
+
+    # Проверить, есть ли alias в query_aliases
+    alias_cursor.execute(
+        "SELECT column_meta_id, column_alias_name FROM column_aliases WHERE column_alias_name = %s;",
+        (alias,)
+    )
+
+    # если есть запись в alias_column - смотрим на alias, если он null - обновляем, иначе - ошибка
+    try:
+        row = alias_cursor.fetchone()
+        if row["column_alias_name"] is not None:
+            raise HTTPException(status_code=400, detail="Alias already exists")
+        else:
+            alias_cursor.execute(
+                "UPDATE column_aliases SET column_alias_name = %s WHERE column_meta_id = %s;",
+                (alias, row["column_meta_id"],)
+            )
+            alias_conn.commit()
+
+    # если нет такой колонки в alias_db - лезем в meta, ищем таблицу по имени и имени базы
+    except TypeError:
+        meta_cursor.execute(
+            "SELECT c.column_id FROM db_columns AS c "
+            "JOIN db_tables AS t ON t.table_id = c.table_id "
+            "JOIN dbs ON dbs.db_id = t.db_id "
+            "WHERE c.column_name = %s AND t.table_name = %s AND dbs.db_name = %s;",
+            (name, table, database, )
+        )
+
+        db_row = meta_cursor.fetchone()
+        if db_row:
+            alias_cursor.execute("SELECT column_meta_id "
+                                 "FROM column_aliases "
+                                 "WHERE column_meta_id = %s",
+                                 (db_row["column_id"], ) )
+
+            if alias_cursor.fetchone():
+                raise HTTPException(status_code=400, detail="Column already has alias")
+
+            alias_cursor.execute(
+                "INSERT INTO column_aliases (column_meta_id, column_alias_name) VALUES (%s, %s);",
+                (db_row["column_id"], alias, )
+            )
+            alias_conn.commit()
+        else:
+            raise HTTPException(status_code=404, detail="Column does not exist")
+
+    meta_cursor.close()
+    alias_cursor.close()
+    meta_conn.close()
+    alias_conn.close()
+
 def create_alias(ar: AliasRequest) -> AliasResponse:
     """
     Creates db/table alias
     """
+    if ar.database != "" and ar.table != "" and ar.column != "":
+        create_column_alias(ar.database, ar.table, ar.column, ar.alias)
+        return AliasResponse(message=(
+            f"Column alias created: database='{ar.database}', table='{ar.table}', column='{ar.column}' alias='{ar.alias}'"
+        ))
 
     # --Создание alias для БД--
     if ar.table == "" and ar.column == "":
         create_db_alias(ar.database, ar.alias)
         return AliasResponse(message=(
-                f"Database alias created: database='{ar.database}', alias='{ar.alias}'"
-            ))
+            f"Database alias created: database='{ar.database}', alias='{ar.alias}'"
+        ))
 
     elif ar.column == "":
         create_table_alias(ar.database, ar.table, ar.alias)
@@ -122,9 +201,10 @@ def create_alias(ar: AliasRequest) -> AliasResponse:
             f"Table alias created: database='{ar.database}', table='{ar.table}', alias='{ar.alias}'"
         ))
 
-    return AliasResponse(message=(
-        f"Something went wrong"
-    ))
+    else:
+        return AliasResponse(message=(
+            f"Please specify database, table and column correctly"
+        ))
 
 
     # # --Создание alias для таблицы БД--
@@ -202,9 +282,9 @@ def delete_alias(ar: AliasRequest) -> AliasResponse:
         cursor.close()
         conn.close()
         return AliasResponse(message=(
-                f"Database alias deleted: database='{db_name}', "
-                f"old_alias='{old_alias}'"
-            ))
+            f"Database alias deleted: database='{db_name}', "
+            f"old_alias='{old_alias}'"
+        ))
 
     # Удаление alias таблицы
     # Проверка существования БД, таблицы и alias
@@ -242,10 +322,10 @@ def delete_alias(ar: AliasRequest) -> AliasResponse:
     )
     conn.commit()
 
-    cursor.close()  
+    cursor.close()
     conn.close()
 
     return AliasResponse(message=(
-            f"Table alias deleted: database='{db_name}', "
-            f"table='{table_name}', old_alias='{table_alias}'"
-        ))
+        f"Table alias deleted: database='{db_name}', "
+        f"table='{table_name}', old_alias='{table_alias}'"
+    ))
